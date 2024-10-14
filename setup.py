c1dca8b9e2f05a5369e5cae00d6a4e72101b78ad
Trenton <130304830+Trentonom0r3@users.noreply.github.com>
2024-10-14 13:48:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="ffmpy",
-    version="0.2.4.1",
+    version="0.2.4.2",
     author="Trenton Flanagan",
     author_email="spigonvids@gmail.com",
     description="HW accelerated video reading for ML Inference.",
